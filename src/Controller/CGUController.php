b80c392e9e537f48f90443cc45a30732ab93dd8b
Mamadou <hamshakour93@gmail.com>
2021-12-24 22:04:14 +0100

    (Feat) Mise en place du projet, gestions des users

---
+++ src/Controller/CGUController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/cgu")
+ */
+
+class CGUController extends AbstractController
+{
+    /**
+     * @Route("/cconditions-generales-utilisations", name="cgu_conditions")
+     */
+    public function index(): Response
+    {
+        return $this->render('cgu/index.html.twig', [
+            'controller_name' => 'CGUController',
+        ]);
+    }
+}
